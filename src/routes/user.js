2793429f8e12308f4fa7a089b890640b43e96e2e
BruceHill <brucehillwalley@gmail.com>
2024-04-01 23:22:28 +0300

    permissions

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,20 +6,20 @@ const router = require('express').Router()
 /* ------------------------------------------------------- */
 // routes/user:
 
-const permissions = require('../middlewares/permissions')
 const user = require('../controllers/user')
+const permissions = require('../middlewares/permissions')
 
 // URL: /users
 
 router.route('/')
-    .get(permissions.isAdmin, user.list)
-    .post(permissions.isStaffOrisAdmin, user.create)
+    .get(permissions.isStaffOrisAdmin, user.list)
+    .post(user.create)
 
 router.route('/:id')
     .get(permissions.isLogin, user.read)
     .put(permissions.isLogin, user.update)
     .patch(permissions.isLogin,user.update)
-    .delete(permissions.isStaffOrisAdmin, user.delete)
+    .delete(permissions.isAdmin, user.delete)
 
 /* ------------------------------------------------------- */
 module.exports = router
